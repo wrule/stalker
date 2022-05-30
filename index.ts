65b929e3bcacc71de65ec448df3ada2b09e033d1
jimao <jimao@jimaodeMacBook-Pro.local>
2022-05-30 18:17:11 +0800

    提交

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { Spot } from '@binance/connector';
 import { Http } from './binance-api/http';
 import secret from './.secret.json';
 
@@ -9,7 +10,16 @@ async function main() {
     secret_key: secret.SECRET_KEY,
   }).post('/sapi/v1/userDataStream');
   if (rsp.status === 200) {
+    const listenKey = rsp.data.listenKey;
     console.log(rsp.data);
+    const client = new Spot(secret.API_KEY, secret.SECRET_KEY, { baseURL: 'https://api2.binance.com' });
+    client.userData(listenKey, {
+      open: () => console.log('open'),
+      close: () => console.log('closed'),
+      message: (json_text: any) => {
+        console.log(json_text);
+      },
+    })
   }
 }
 
